82a481c1ce065898f0ffb67c8e583a8ebf8d9559
sorinupwork <sorin.upwork@gmail.com>
2023-07-08 15:36:30 +0300

    ready to deploy blog-nextjs-hygraph

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import React from "react";
+
+import { Layout } from "@/components";
+import "@/styles/globals.scss";
+import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
